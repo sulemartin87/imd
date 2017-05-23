0adf82767d4e63056f1b6ecde5fee5a856e9c2ea
Martin Suleman <sulemartin87@gmail.com>
2017-05-24 01:42:57 +0200

    second commit

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
   <!-- Project Section -->
   <div class="w3-container w3-padding-32" id="projects">
   
-    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Projects</h3>
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Featured Projects</h3>
   </div>
 
     
